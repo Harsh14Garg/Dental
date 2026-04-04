60e2b453efd21798a3cf644a591c80694cd368d2
Harsh14Garg <gargharsh565@gmail.com>
2026-04-04 17:16:57 +0530

    Remove ErrorBoundary from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import ErrorBoundary from './components/ErrorBoundary';
 import './index.css';
 
 // Suppress benign WebSocket errors in the preview environment
@@ -22,8 +21,6 @@ if (typeof window !== 'undefined') {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </StrictMode>,
 );
